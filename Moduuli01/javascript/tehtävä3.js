7eae0b652471e26e76eb475fa9ed93d9edccbe8e
sarapappila <sara.pappila@metropolia.fi>
2023-03-31 18:14:26 +0300

    Tehty

--- Moduuli01/javascript/tehtävä3.js
+++ Moduuli01/javascript/tehtävä3.js
@@ -1,7 +1,7 @@
 'use strict';
-    let first = prompt("Anna luku.")
-    let second = prompt("Anna toinen luku.")
-    let third = prompt("Anna kolmas luku.")
+    let first = prompt("Anna luku.");
+    let second = prompt("Anna toinen luku.");
+    let third = prompt("Anna kolmas luku.");
     let sum = +first + +second + +third
     let product = first * second * third
     let average = (first * second * third) / 3
